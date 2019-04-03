6f96d9555f5a9d65c7b09025c9caa7e22105f051
Cory Gargas <cbgargas@uark.edu>
2019-04-03 14:29:14 -0500

    added arg.parse to script to allow cmdline input

--- parse_gff.py
+++ parse_gff.py
@@ -1,34 +1,78 @@
 #! /usr/bin/python 
+import argparse
 
-# Specify the input files
-fasta_file = "/home/cbg/2019s/biol5153/watermelon_files/watermelon.fsa"
-gff_file = "/home/cbg/2019s/biol5153/watermelon_files/watermelon.gff"
+#create and argumentparser object('parser') that will hold all info to parse the cmd line
+parser = argparse.ArgumentParser(description = 'extracts sequences from a genome based on features in a .gff file')
+
+#adding positional arguments to parser
+parser.add_argument('fasta_file', help='.fasta file you wish to parse by .gff features', type=str)
+parser.add_argument('gff_file', help='.gff file that you will use to parse your .fasta genome file', type=str)
+
+#parse the cmd line arguments
+args = parser.parse_args()
 
 # open the fasta file
-genome = open(fasta_file, "r")
+fasta = open(args.fasta_file, "r")
+genome=fasta.read().rstrip('\n')
+header,seq= genome.split('\n')
+#print(seq)
 
 # open the gff file
-gff=open(gff_file, "r")
+gff=open(args.gff_file, "r")
+
+#print header info
+print(header)
 
 # read the gff file line by line
 for line in gff:
-	#skip blank lines
-	
+
 	#removing line breaks
 	line = line.rstrip('\n')
 	#print(line)
 	#split each line on the tab character
 	sequence, source, feature, start, end, length, strand, phase, attributes = line.split('\t')
-	print(start, end)
-	
+
+	#print(start, end)
+	#print(start_n, end_n)
+
 	#extract the DNA seq from the genome
 	#.gff files are from 1, so we need to either add or subtract one to get it to grab the right seq bits
-	for char in genome:	
-		substring=genome[(int(start)-1):(int(end)+1)]
+	for char in seq:	
+
+		#define feature coordinates for python
+		start_n=int(start)-1
+		end_n=int(end)+1
+		
+		#create substring to pull feeature from
+		substring=seq[start_n:end_n]
+		
+		#print feature coordinates
+		print(start+':'+end)
+		
+		#print the DNA seq of this feature
 		print(substring)
-	
-	#print the DNA seq of this feature
-	
+
 	# calculate the GC content for this feature
-	
-#gff.close()
+		#calculates length of substring
+		sequence_length = len(substring)
+
+	#calculates number of respective nucleotide in substring
+		#numA = substring.count('A')
+		numC = substring.count('C')
+		numG = substring.count('G')
+		#numT = substring.count('T')
+
+	#calulates freq of A,C,G,T
+		#freqA = ((numA/sequence_length)*100)
+		freqC = ((numC/sequence_length)*100)
+		freqG = ((numG/sequence_length)*100)
+		#freqT = ((numT/sequence_length)*100)
+
+	#print the output
+		#print('freq of A: %.2f' % freqA)
+		print('freq of G: %.2f,' % freqG, 'freq of C: %.2f' % freqC)
+		#print('freq of T: %.2f' % freqT)
+		break		
+#close the files 		
+gff.close()
+fasta.close()
